fb212427295034aae3b52acc9f62dbd61ca0ffe3
sunitsen <69672778+sunitsen@users.noreply.github.com>
2022-10-12 13:45:05 +0600

    message

---
+++ assets/vandor/jquary/main.js
@@ -0,0 +1,8 @@
+//template sidebar nav
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
